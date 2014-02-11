a9065eade29beb8c03c26ac8b83720a56829bb67
Carsten Munk <carsten.munk@jollamobile.com>
2014-02-11 16:04:17 +0000

    Revert "hooks: search in librt too"
    This reverts commit 30f6f5ef8366ec18eab496c24556e6dfd6e20571.

--- hybris/common/hooks.c
+++ hybris/common/hooks.c
@@ -1429,12 +1429,6 @@ void *get_hooked_symbol(char *sym)
         return rv;
     }
 
-    rv = hook_add_from_lib(sym, "librt.so");
-    if (rv != NULL) {
-        pthread_mutex_unlock(&hook_mutex);
-        return rv;
-    }
-
     rv = hook_add_from_lib(sym, "libc.so.6");
     if (rv != NULL) {
         pthread_mutex_unlock(&hook_mutex);
